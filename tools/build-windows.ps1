7f02304e3e5218a5eaa36ca3e92d8492467411e9
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-11-27 22:24:04 +0100

    ADD: update submodule in windows build Replaced git pull with submodule update

--- tools/build-windows.ps1
+++ tools/build-windows.ps1
@@ -1,7 +1,7 @@
 $ScriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 cd $SCRIPTPATH/..
-git pull --recurse-submodules
+git submodule update
 cmake -B build .
 cd build
-cmake --build . -j $env:NUMBER_OF_PROCESSORS 
+cmake --build . -j $env:NUMBER_OF_PROCESSORS
