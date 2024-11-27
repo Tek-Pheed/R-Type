9301956c6fd078e1e185408dbd249c9d8ee81ea3
Alexandre <alexandre.ricard@epitech.eu>
2024-11-27 19:33:45 +0100

    ADD: windows build tool
    adding build-windows.ps1 in tools folder

--- .github/ci_scripts/run_make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cmake . && cmake -B build -S . && make -C build > temp.txt 2>&1 || (echo "::error title=Build Checker Linux::Build Failed" && exit 1)
---
+++ tools/build-windows.ps1
@@ -0,0 +1,7 @@
+$ScriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+cd $SCRIPTPATH/..
+git pull --recurse-submodules
+cmake -B build .
+cd build
+cmake --build .
