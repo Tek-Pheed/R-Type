2b4723eed48fbf015a25f302cec68192ed9722d9
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-12-01 15:56:17 +0100

    ADD: new libSystem server cpp example

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -17,11 +17,11 @@
 
 static volatile std::atomic<bool> stop = false;
 
-//void sighandler(int n)
+// void sighandler(int n)
 //{
-//    (void) n;
-//    stop = true;
-//}
+//     (void) n;
+//     stop = true;
+// }
 
 // Example TCP Server with libSystem
 int main(void)
@@ -35,7 +35,18 @@ int main(void)
     System::Network::timeoutStruct tv;
     tv = {1, 0};
     clients.clear();
-    //signal(SIGINT, &sighandler);
+    // signal(SIGINT, &sighandler);
+
+    std::cout << "Connecting to self socket" << std::endl;
+    try {
+        System::Network::TCPSocket testSocket(
+            1234, System::Network::TCPSocket::CONNECT, "127.0.0.1");
+        testSocket.sendData(
+            System::Network::byteArray({'h', 'e', 'l', 'l', 'o'}));
+        testSocket.closeSocket();
+    } catch (const std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+    }
 
     try {
         std::cout << "Started TCP Server on Port 1234" << std::endl;
@@ -47,7 +58,7 @@ int main(void)
 
             std::cout << "Set size: " + std::to_string(socketSetTCP.size())
                       << std::endl;
-            System::Network::select(&socketSetTCP, nullptr, nullptr, tv);
+            System::Network::select(&socketSetTCP, nullptr, nullptr);
 
             std::cout << "Select event" << std::endl;
 
@@ -63,11 +74,14 @@ int main(void)
                     std::cout << "Reading from client: "
                               << System::Network::decodeString(sock->receive())
                               << std::endl;
-                    //sock->send(System::Network::byteArray({'h','e', 'l', 'l', 'o'}));
+                    if (sock->isOpen())
+                        sock->sendData(System::Network::byteArray({'h','e', 'l',
+                        'l', 'o'}));
                     if (!sock->isOpen()) {
                         std::cout << "Removing client" << std::endl;
                         sock->closeSocket();
-                        System::Network::removeSocketInSet(*sock, socketSetTCP);
+                        System::Network::removeSocketInSet(
+                            *sock, socketSetTCP);
                         System::Network::removeSocketInVect(*sock, clients);
                     }
                 }
