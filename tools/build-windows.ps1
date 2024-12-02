e3390eaed7a9c4a39a5c4516560c134fd3a72799
SlyMoz <samynasset84@gmail.com>
2024-12-02 12:28:03 +0100

    FIX: windows compilation Windows compilation with CMake in /release

--- tools/build-windows.ps1
+++ tools/build-windows.ps1
@@ -5,3 +5,14 @@ git submodule update --init --recursive
 cmake -B build .
 cd build
 cmake --build . -j $env:NUMBER_OF_PROCESSORS
+
+Write-Host "Listing contents of build directory:"
+ls
+
+$sourceDir = "dependencies/SFML/"
+$destDir = "../release/"
+
+Get-ChildItem -Path $sourceDir -Include *.dll, *.recipe -Recurse | ForEach-Object {
+    Write-Host "Copying file: $_.FullName"
+    Copy-Item -Path $_.FullName -Destination $destDir
+}
