364cf5a7ef8b2c0ce6d36add40dc4d26497a776e
Arnaud Augait <arnaud.augait@epitech.eu>
2024-12-02 10:01:50 +0100

    ADD: change function getConfig and private variable

--- client/include/Config.hpp
+++ client/include/Config.hpp
@@ -2,16 +2,21 @@
 #define CONFIGCLASS_HPP_
 
 #include <iostream>
+#include <fstream>
+#include <map>
 
 class Config {
     public:
-        Config();
+        Config(const std::string &filename);
         ~Config();
-        void getConfig();
-        void setConfig(line: int, param: string);
+        const std::map<int, std::string>& getConfig();
+        void setConfig(int line, const std::string &param);
         void saveConfig();
         void parseConfig();
     private:
+        std::string _filename;
+        std::map<int, std::string> _configData;
+
 };
 
 #endif /* !CONFIGCLASS_HPP_ */
