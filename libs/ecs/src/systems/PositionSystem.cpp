3916151fc776d350a08f3d55a5442ed2a4916f69
SlyMoz <samy.nasset@epitech.eu>
2024-12-04 14:43:54 +0100

    ADD: position system

---
+++ libs/ecs/src/systems/PositionSystem.cpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type [WSL: Ubuntu-24.04]
+** File description:
+** PositionSystem
+*/
+
+#include "Systems.hpp"
+
+ecs::PositionSystem::PositionSystem()
+{
+}
+
+void ecs::PositionSystem::update(std::vector<std::unique_ptr<ecs::Entity>> &entities, sf::RenderWindow *window, float deltaTime)
+{
+    for (auto &entity : entities) {
+        auto positionComponent = entity.get()->getComponent<ecs::PositionComponent>();
+        auto velocityComponent = entity.get()->getComponent<ecs::VelocityComponent>();
+        if (positionComponent && velocityComponent) {
+
+            float newX = static_cast<float>(positionComponent->getX()) + static_cast<float>(velocityComponent->getVx()) * deltaTime;
+            float newY = static_cast<float>(positionComponent->getY()) + static_cast<float>(velocityComponent->getVy()) * deltaTime;
+
+            auto windowSize = window->getSize();
+            float windowWidth = static_cast<float>(windowSize.x);
+            float windowHeight = static_cast<float>(windowSize.y);
+
+            if (newX >= 0 && newX <= windowWidth) {
+                positionComponent->setX(static_cast<int>(newX));
+            }
+            if (newY >= 0 && newY <= windowHeight) {
+                positionComponent->setY(static_cast<int>(newY));
+            }
+        }
+    }
+}
