1870fccf75e564a5cfaf363d1769fa704471ad3c
LucasLoustalot <lucas.loustalot@epitech.eu>
2024-12-18 15:13:20 +0100

    ADD: asset manager in game engine Added an asset manager in tinyV8

--- libs/tinyV8/include/AssetManager.hpp
+++ libs/tinyV8/include/AssetManager.hpp
@@ -9,38 +9,65 @@
 #define TINY_V8_ASSET_MANAGER
 
 #include <any>
-#include <memory>
+#include <utility>
 #include "Engine.hpp"
 #include <unordered_map>
 
-// namespace Engine
-// {
-//     namespace Feature
-//     {
+namespace Engine
+{
+    namespace Feature
+    {
 
-//         class AssetManager : public AEngineFeature {
-//           public:
-//             explicit AssetManager(Core &engineRef);
-//             ~AssetManager();
+        class AssetManager : public AEngineFeature {
+          public:
+            explicit AssetManager(Core &engineRef);
+            ~AssetManager();
 
-//             template<typename BackendType>
-//             BackendType &loadAsset(const std::string &path, const std::string &identifier);
+            template <typename BackendType>
+            BackendType &getAsset(const std::string &identifier)
+            {
+                BackendType *object = std::any_cast<BackendType>(
+                    &(_assets[typeid(BackendType)].at(identifier)));
+                return (*object);
+            }
 
-//             template<typename BackendType>
-//             BackendType &getAsset(const std::string &path, const std::string &identifier);
+            template <typename BackendType, typename LoadFncType,
+                typename... Args>
+            BackendType &loadAsset(const std::string &path,
+                const std::string &identifier,
+                LoadFncType(BackendType::*function), Args... extraArgs)
+            {
+                _assets[typeid(BackendType)].insert(
+                    std::make_pair<std::string, BackendType>(
+                        std::string(identifier), BackendType()));
+                BackendType *object = std::any_cast<BackendType>(
+                    &(_assets[typeid(BackendType)].at(identifier)));
+                ((object)->*function)(path, extraArgs...);
+                return (*object);
+            }
 
-//           protected:
-//             void onStart(void) override;
-//             void onTick(float deltaTimeSec) override;
-//             void onStop(void) override;
+            template <typename BackendType>
+            bool unloadAsset(const std::string &identifier)
+            {
+                if (!_assets.contains(typeid(BackendType))) {
+                    return (false);
+                }
+                _assets[typeid(BackendType)].erase(identifier);
+                return (true);
+            }
 
-//             std::unordered_map<std::type_index, std::unordered_map<std::string, std::any>> _assets;
+          protected:
+            void onStart(void) override;
+            void onTick(float deltaTimeSec) override;
+            void onStop(void) override;
 
-//             //std::unordered_map<std::string, std::unique_ptr<BackendTypes>...> _content;
-//         };
+            std::unordered_map<std::type_index,
+                std::unordered_map<std::string, std::any>>
+                _assets;
+        };
 
-//     } // namespace Feature
+    } // namespace Feature
 
-// } // namespace Engine
+} // namespace Engine
 
 #endif /* TINY_V8_ASSET_MANAGER */
